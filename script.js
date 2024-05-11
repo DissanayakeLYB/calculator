89f8af3905eaac7cf286a01d30f9c82cbda39808
Lasith Dissanayake <86358728+DissanayakeLYB@users.noreply.github.com>
2024-05-11 13:42:18 +0530

    .

--- script.js
+++ script.js
@@ -1,33 +1,25 @@
-function CE(){
-    return;
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
 }
-function num_1(){
-    return 1;
+
+function deleteOnce(){
+    (display.value).pop()
+    display.value
 }
-function num_2(){
-    return 2;
+
+function clearAll(){
+    display.value = " ";
 }
-function num_3(){
-    return 3;
+
+function calculate(){
+    display.value = eval(display.value)
 }
-function num_4(){
-    return 4;
-}
-function num_5(){
-    return 5;
-}
-function num_6(){
-    return 6;
-}
-function num_7(){
-    return 7;
-}
-function num_8(){
-    return 8;
-}
-function num_9(){
-    return 9;
-}
-function num_0(){
-    return 0;
+
+function minus(){
+    if ((display.value)[0] == "-"){
+        (display.value)[0] = " ";
+    }
+    display.value;
 }
